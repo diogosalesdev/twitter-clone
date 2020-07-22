7fbe22952e825ec31c49048e5a887a783e5ce1ee
diogosalesdev <diogosalesdev@gmail.com>
2020-07-21 21:36:05 -0300

    Creating screen divisors

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Container, Header, BackIcon, ProfileInfo } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong>Deia Ribeiro</strong>
+          <span>612 Tweets</span>
+        </ProfileInfo>
+      </Header>   
+      {/* <ProfilePage /> */}
+      {/* <BottomMenu>
+        <HomeIcon />
+        <SearchIcon />
+        <BellIcon />
+        <EmailIcon />
+      </BottomMenu> */}
+    </Container>
+  );
+}
+
+export default Main;
---
+++ src/styles/Icons.ts
@@ -0,0 +1,12 @@
+export {
+  Home,
+  Notifications,
+  Email,
+  FavoriteBorder,
+  ExitTopApp,
+  LocationOn,
+  Search
+} from 'styled-icons/material-outlined';
+export {Person, Cake, Favorite} from 'styled-icons/material';
+export {ArrowLeft} from 'styled-icons/heroicons-solid';
+export {Chat} from 'styled-icons/heroicons-outline';
